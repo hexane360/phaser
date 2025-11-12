38e18b0d92f71d861c1bf0ee07d5bf6ae3de9747
bonnielin0134 <yxlin@mit.edu>
2025-11-12 18:23:48 -0500

    Include max shift in the align_object_to_ground_truth warning

--- phaser/utils/analysis.py
+++ phaser/utils/analysis.py
@@ -228,9 +228,11 @@ def align_object_to_ground_truth(
     ))
     if max_shift < 0:
         raise ValueError("Error: Ground truth extent smaller than object extent")
-    elif max_shift * ground_truth_samp.sampling[0] < 10:
-        warnings.warn("Ground truth extent is only slightly larger than the object extent, which might not be enough for cross-correlation to find the best alignment. ")
-    #print(f"max_shift: {max_shift:.3f} px ({max_shift * ground_truth_samp.sampling[0]:.3f} angstrom)")
+    elif max_shift * ground_truth_samp.sampling[0] < 10.0:  # 10 A = 1 nm
+        warnings.warn(
+            "Ground truth extent is only slightly larger than the object extent, which might not be enough for cross-correlation to find the best alignment.\n"
+            f"Maximum shift is limited to {max_shift:.3f} px ({max_shift * ground_truth_samp.sampling[0]:.3f} angstrom)"
+        )
 
     shift = numpy.array(_cross_correlate(upsamp_obj[tuple(crop)], ground_truth[tuple(crop)], max_shift))
 
